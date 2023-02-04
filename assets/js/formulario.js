98f0dfdad0cf7e9d59450d1647583259a4601428
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-02-03 21:15:22 -0300

    estilo do formulario pronta, tentando adicionar a troca de formulario

---
+++ assets/js/formulario.js
@@ -0,0 +1,7 @@
+let login = document.querySelector('.login');
+let criar = document.querySelector('.criar');
+let formulario = document.querySelector('.formulario');
+
+login.onclick = function(){
+    formulario.classList.add('signinForm');
+}
