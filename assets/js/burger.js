5f3952c1ad48b068bfba0d0883a180c637c404d4
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-02-05 14:03:42 -0300

    ]

--- assets/js/burger.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector(".burger");
-const barmenu = document.querySelector(".bar__menu");
-
-burger.addEventListener("click", () => {
-    burger.classList.toggle('active');
-    barmenu.classList.toggle('active')
-})
